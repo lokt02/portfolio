9906b1aee79bea4086e14279088c7873d8800037
Sergei <lokt02@mail.ru>
2021-11-01 13:17:21 +0300

    Minor Update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -16,7 +16,7 @@ const Header = () =>  (
     </Div1>
     <Div2>
       <li>
-        <Link href="#projects">
+        <Link href="/projects">
           <NavLink>Projects</NavLink>
         </Link>
       </li>
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -3,18 +3,14 @@ import Projects from '../components/Projects/Projects';
 import Technologies from '../components/Technologies/Technologies';
 import Timeline from '../components/TimeLine/TimeLine';
 import { Layout } from '../layout/Layout';
-import { Section } from '../styles/GlobalComponents';
+import { Section, SectionDivider, SectionText, SectionTitle } from '../styles/GlobalComponents';
 
-const Home = () => {
+const ProjectsPage = () => {
   return (
     <Layout>
-      <Section grid>
-        <Hero />
-      </Section>
-      <Timeline />
-      <Technologies />
+      <Projects />
     </Layout>
   );
 };
 
-export default Home;
+export default ProjectsPage;
